2fff0755a87238bcde47dbae941f8f9ef6196511
capirela-duoc <ca.pirela@duocuc.cl>
2026-07-11 18:15:34 -0400

    fix: correcciones logicas

--- main.py
+++ main.py
@@ -73,32 +73,10 @@ def unidades_categoria(categoria ,d_prendas, dicc_bodega):
 
 
 def eliminar_prenda(codigo, d_prendas, d_bodega):    
-    if not d_prendas or not d_bodega or len(d_prendas) == 0 :
-            print('No hay registros en nuestro sistema. Volverá al menú principal.')
-            return
-    else:
-        while True:
-            codigo = input('Ingrese código de prenda que desea eliminar: ').upper()
-        
-            if not validar_texto(codigo):
-                print('Error al ingresar el código de la prenda. Favor intente de nuevo.')
-                
-            else:
-                busqueda_codigo = buscar_codigo(codigo, d_bodega)
-                if not busqueda_codigo:
-                    print('El código ingresado no coincide con nuestros registros. Favor intente de nuevo.')
-                else:
-                    prenda = d_prendas[codigo][0]
-                    opcion = input(f'¿Está seguro desea eliminar el prenda {prenda} del sistema? S/N: ')
-                    
-                    if not validar_opcion(opcion):
-                        break
-                    else:
-                        d_prendas.pop(codigo)
-                        d_bodega.pop(codigo)
-                        
-                        print(f"Producto con código {codigo} eliminado exitosamente.")
-                        break 
+    d_prendas.pop(codigo)
+    d_bodega.pop(codigo)
+    
+    return True
 
 def busqueda_precio(p_min, p_max, d_prendas, d_bodega):
     try:
@@ -134,137 +112,16 @@ def busqueda_precio(p_min, p_max, d_prendas, d_bodega):
         print(f'Ha ocurrido un error en el sistema. Error: {error}')
 
 
-def actualizar_precio(codigo, nuevo_precio, d_prendas, d_bodega):
-    try:
-        if not d_prendas or not d_bodega or len(d_prendas) == 0 :
-            print('No hay registros en nuestro sistema. Volverá al menú principal.')
-            return
-        while True:
-            codigo = input('Favor ingrese código de prenda cuyo precio desea actualizar: ').upper()
-            
-            if not validar_texto(codigo):
-                print('Error al ingresar el código del prenda. Favor intente de nuevo.')
-                
-            if not buscar_codigo(codigo, d_prendas):
-                print('El código ingresado no coincide con nuestros registros. Favor intente de nuevo')
-                
-            else:
-                validar_actualizacion = input(f'El nuevo_precio del prenda {d_prendas[codigo][0]} es de {d_bodega[codigo][0]}. ¿Está seguro que desea actualizar? S/N: ')
-                
-                if validar_opcion(validar_actualizacion):
-                    nuevo_precio = int(input('Favor ingrese nuevo nuevo_precio: '))
-                    
-                    d_bodega[codigo][0] = nuevo_precio
-                    
-                    print(f'El nuevo_precio de la prenda "{d_prendas[codigo][0]}", ha sido actualizado correctamente a {d_bodega[codigo][0]}')
-                    return True
-                else:
-                    print('Usted ha elegido no actualizar el nuevo_precio.')
-                    return False
-    
-    except ValueError as error:
-        print(f'Ha ocurrido un error al actualizar el nuevo_precio. Error: {error}')
+def actualizar_precio(codigo, nuevo_precio, d_bodega):
+    d_bodega[codigo][0] = nuevo_precio                
+    return True
 
 def agregar_prenda(codigo, nombre, categoria, talla, color, material, es_unisex, precio, unidades, d_prendas, d_bodega):        
-    try:
-
-        while True:
-            codigo = input('Ingrese código de producto: ').upper()
-
-            if not validar_texto(codigo):
-                print('Error al ingresar el código del producto. FAvor intente de nuevo.')
-            elif buscar_codigo(codigo, d_prendas):
-                print("El código ingresado ya existe. Favor intente d nuevo.")
-            else:
-                break
-
-
-        while True:
-            nombre = input('Ingrese nombre del prendamento: ')
-            
-            if not validar_texto(nombre):
-                print('Error al ingresar el nombre. Favor ingrese de nuevo')
-            else:
-                break
+    if categoria or nombre or categoria or talla or color or es_unisex or precio or unidades:
+        d_prendas[codigo] = [nombre, categoria, talla, color, material, es_unisex]
+        d_bodega[codigo] = [precio, unidades]
         
-        while True:
-            categoria = input('Ingrese la categoria del prendamento: ')
-            
-            if not validar_texto(categoria):
-                print('Error al ingresar el categoria. Favor ingrese de nuevo')
-            else:
-                break
-        
-        while True:
-            material = input('Ingrese el material del prendamento: ')
-            
-            if not validar_texto(material):
-                print('Error al ingresar el material. Favor ingrese de nuevo')
-            else:
-                break
-            
-        while True:
-            talla = input('Ingrese el talla del prendamento: ')
-            
-            if not validar_texto(talla):
-                print('Error al ingresar el talla. Favor ingrese de nuevo')
-            else:
-                break
-            
-        while True:
-            color = input('Ingrese el color de la prendamento: ')
-            
-            if not validar_texto(color):
-                print('Error al ingresar el color. Favor ingrese de nuevo')
-            else:
-                break
-        
-        while True:
-            validar_es_unisex = input('Favor indique si la prenda es unisex. Ingrese "s" para si y "n" para no: ' )
-            
-            if validar_es_unisex.strip().upper() not in ("S", "N"):
-                    print('Error al indicar si es unisex. Favor ingrese de nuevo')
-            else:
-                es_unisex = validar_opcion_unisex(validar_es_unisex)
-                break
-            
-        while True:
-            try:
-                precio = int(input('Favor ingrese precio del prendamento: '))
-                
-                if not validar_entero_positivo(precio):
-                    print('Error al ingresar precio. Favor intente de nuevo')
-                else:
-                    break
-            
-            except ValueError as error:
-                print(f'Ha ocurrido un error al ingresar el precio del prendamento. Error: {error}')
-                
-        while True:
-            try:
-                unidades =int(input('Favor cantidad de unidades disponibles del prendamento: '))
-                
-                if not  validar_entero_mayor_cero(unidades):
-                    print('Error las unidades. Favor intente de nuevo')
-                else:
-                    break
-            
-            except ValueError as error:
-                print(f'Ha ocurrido un error al ingresar las unidades del prendamento. Error: {error}')
-                
-        
-            if categoria or nombre or categoria or talla or color or es_unisex or precio or unidades:
-                d_prendas[codigo] = [nombre, categoria, talla, color, material, es_unisex]
-                d_bodega[codigo] = [precio, unidades]
-                
-                print('Prenda ingresada correctamente')
-                print(f'Prenda: {d_prendas}')
-                print(f'Bodega: {d_bodega}')
-                    
-            
-    except ValueError as error:
-        print(f'Ha ocurrido un error la ingresar el producto en sistema. Favor intente de nuevo. Error: {error}')
-
+        return True
     
 
 ############ FIN CONTROLLERS ###############
@@ -272,10 +129,11 @@ def agregar_prenda(codigo, nombre, categoria, talla, color, material, es_unisex,
 def main():
     def leer_opcion():
         try:
-            if opMenu is None or (opMenu < 1 or opMenu > len(menu)):
-                return False
+            opMenu = int(input('Favor elija una de las opciones para continuar: '))
+            if 1 <= opMenu <= 6:
+                return opMenu
             else:
-                return True
+                print('Error al ingresar opción. No coincide con nuestros registros')
         except ValueError as error:
             print(f'Error al ingresar opción. Error: {error}')
 
@@ -292,53 +150,196 @@ def main():
             'S006': ['Camisa Formal', 'camisa', 'M', 'blanco', 'algodon',
             False],
         }
-        bodega = {}
+        bodega = {
+                'S001': [7990, 12],
+                'S002': [19990, 0],
+                'S003': [29990, 3],
+                'S004': [24990, 6],
+                'S005': [17990, 8],
+                'S006': [14990, 2],
+        }
         menu = ['Unidades por categoría', 'Búsqueda de prendas por rango de precio', 'Actualizar precio de prenda', 'Agregar prenda', 'Eliminar prenda', 'Salir']
+        
         opMenu = 0
 
         while opMenu != 6:
-            mostrar_menu(menu)
-            opMenu = int(input('Favor elija una de las opciones para continuar: '))
+            mostrar_menu(menu)      
             
+            opMenu = leer_opcion()      
             
-            if not leer_opcion(): 
-                print('La opción ingresada no coincide con nuestro sistema. Favor intente de nuevo.')
-            
-            else:
-                match opMenu:
-                    case 1:
-                        categoria = ""
-                        unidades_categoria(categoria, prendas, bodega)  
-                    case 2:
-                        p_min = 0
-                        p_max = 0
+            match opMenu:
+                case 1:
+                    categoria = ""
+                    unidades_categoria(categoria, prendas, bodega)  
+                case 2:
+                    p_min = 0
+                    p_max = 0
 
-                        busqueda_precio(p_min, p_max, prendas, bodega)
-                    case 3:
-                        codigo = ""
-                        nuevo_precio = 0
+                    busqueda_precio(p_min, p_max, prendas, bodega)
+                case 3:
+                    codigo = ""
+                    nuevo_precio = 0
 
-                        actualizar_precio(codigo, nuevo_precio, prendas, bodega)
-                    case 4:
-                        codigo = ""
-                        nombre = ""
-                        categoria = ""
-                        talla = "" 
-                        color = ""
-                        material = ""
-                        es_unisex = False
-                        precio = 0
-                        unidades = 0
+                    if not prendas or not bodega or len(prendas) == 0 :
+                        print('No hay registros en nuestro sistema. Volverá al menú principal.')
+                        continue
+                        
+                    codigo = input('Favor ingrese código de prenda cuyo precio desea actualizar: ').upper()
+                    
+                    if not validar_texto(codigo):
+                        print('Error al ingresar el código del prenda. Favor intente de nuevo.')
+                        continue
+                        
+                    if not buscar_codigo(codigo, prendas):
+                        print('El código ingresado no coincide con nuestros registros. Favor intente de nuevo')
+                        continue
+                        
+                    validar_actualizacion = input(f'El nuevo_precio del prenda {prendas[codigo][0]} es de {bodega[codigo][0]}. ¿Está seguro que desea actualizar? S/N: ')
+                    
+                    if validar_opcion(validar_actualizacion):
+                        try:
+                            nuevo_precio = int(input('Favor ingrese nuevo nuevo_precio: '))
+                            
+                            if validar_entero_positivo(nuevo_precio):
+                                actualizar_precio(codigo, nuevo_precio, bodega)
+                                print('Precio actualizado correctamente.')
+                            else:
+                                print('El precio debe ser mayor a cero.')
+                                continue
+                        
+                        except ValueError as error:
+                            print(f'Error: {error}')  
+                    else:
+                        print('Usted ha elegido no actualizar el nuevo_precio.')
+                            
+                case 4:
+                    codigo = ""
+                    nombre = ""
+                    categoria = ""
+                    talla = "" 
+                    color = ""
+                    material = ""
+                    es_unisex = False
+                    precio = 0
+                    unidades = 0
+                    
+                    
+                    while True:
+                        codigo = input('Ingrese código de producto: ').upper()
 
-                        agregar_prenda(codigo, nombre, categoria, talla, color, material, es_unisex, precio, unidades, prendas, bodega)
-                    case 5:
-                        codigo = ""
-                        eliminar_prenda(codigo, prendas, bodega)
-                    case 6:
-                        print('Usted ha elegido salir del sistema!')
-                        return
-                    case _: 
-                        print('La opción ingresada no coincide con nuestro sistema. Favor intente de nuevo.')
+                        if not validar_texto(codigo):
+                            print('Error al ingresar el código del producto. FAvor intente de nuevo.')
+                        elif buscar_codigo(codigo, prendas):
+                            print("El código ingresado ya existe. Favor intente d nuevo.")
+                        else:
+                            break
+
+
+                    while True:
+                        nombre = input('Ingrese nombre del prendamento: ')
+                        
+                        if not validar_texto(nombre):
+                            print('Error al ingresar el nombre. Favor ingrese de nuevo')
+                        else:
+                            break
+                    
+                    while True:
+                        categoria = input('Ingrese la categoria del prendamento: ')
+                        
+                        if not validar_texto(categoria):
+                            print('Error al ingresar el categoria. Favor ingrese de nuevo')
+                        else:
+                            break
+                    
+                    while True:
+                        material = input('Ingrese el material del prendamento: ')
+                        
+                        if not validar_texto(material):
+                            print('Error al ingresar el material. Favor ingrese de nuevo')
+                        else:
+                            break
+                        
+                    while True:
+                        talla = input('Ingrese el talla del prendamento: ')
+                        
+                        if not validar_texto(talla):
+                            print('Error al ingresar el talla. Favor ingrese de nuevo')
+                        else:
+                            break
+                        
+                    while True:
+                        color = input('Ingrese el color de la prendamento: ')
+                        
+                        if not validar_texto(color):
+                            print('Error al ingresar el color. Favor ingrese de nuevo')
+                        else:
+                            break
+                    
+                    while True:
+                        validar_es_unisex = input('Favor indique si la prenda es unisex. Ingrese "s" para si y "n" para no: ' )
+                        
+                        if validar_es_unisex.strip().upper() not in ("S", "N"):
+                                print('Error al indicar si es unisex. Favor ingrese de nuevo')
+                        else:
+                            es_unisex = validar_opcion_unisex(validar_es_unisex)
+                            break
+                        
+                    while True:
+                        try:
+                            precio = int(input('Favor ingrese precio del prendamento: '))
+                            
+                            if not validar_entero_positivo(precio):
+                                print('Error al ingresar precio. Favor intente de nuevo')
+                            else:
+                                break
+                        
+                        except ValueError as error:
+                            print(f'Ha ocurrido un error al ingresar el precio del prendamento. Error: {error}')
+                            
+                    while True:
+                        try:
+                            unidades =int(input('Favor cantidad de unidades disponibles del prendamento: '))
+                            
+                            if not  validar_entero_mayor_cero(unidades):
+                                print('Error las unidades. Favor intente de nuevo')
+                            else:
+                                break
+                        except ValueError as error:
+                            print(f'Ha ocurrido un error al ingresarlas unidades de la prenda. Error: {error}')
+                    
+                    if agregar_prenda(codigo, nombre, categoria, talla, color, material, es_unisex, precio, unidades, prendas, bodega):
+                        print('Prenda ingresada correctamente')
+                case 5:
+                    codigo = ""
+                    
+                    if not prendas or not bodega or len(prendas) == 0 :
+                        print('No existen registros ene nuestro sistema. Volverá al menú principal')
+                        continue
+                    
+                    codigo = input('Ingrese código de prenda que desea eliminar: ').upper()
+                    
+                    if not validar_texto(codigo):
+                        print('Error al ingresar el código de la prenda. Favor intente de nuevo.')
+                    elif not buscar_codigo(codigo, bodega):
+                        print('El código ingresado no coincide con nuestros registros. Favor intente de nuevo.')
+                    else:
+                        prenda = prendas[codigo][0]
+                        opcion = input(f'¿Está seguro desea eliminar el prenda {prenda} del sistema? S/N: ')
+                        
+                        if not validar_opcion(opcion):
+                            break
+                        else:
+                            eliminacion = eliminar_prenda(codigo, prendas, bodega)
+                            if eliminacion:
+                                print('Prenda eliminada existosamente.')
+                            else: 
+                                print('Usted ha elegido no eliminar prenda')
+                        
+                case 6:
+                    print('Usted ha elegido salir del sistema!')
+                    return
+                case _: 
+                    print('La opción ingresada no coincide con nuestro sistema. Favor intente de nuevo.')
     except ValueError as error:
         print(f'Ha ocurrido un error en el sistema. Error: {error}')
 main()
